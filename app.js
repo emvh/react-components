9662cb459a3e9fe7501d73670e036f3d839afb52
emvh <emilyvho@gmail.com>
2020-05-26 11:29:29 -0700

    Create 2 grocery list items as components and use inside GroceryList instead of hardcoded list items

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // TODO
+var Cheese = () => (
+  <div>Cheese</div>
+);
+
+var ChickenNuggets = () => (
+  <div>Chicken Nuggets</div>
+);
+
+var GroceryList = () => (
+  <div>
+    <h3> My Grocery List</h3>
+      <Cheese />
+      <ChickenNuggets />
+  </div>
+);
+ ReactDOM.render(<GroceryList />, document.getElementById('app'));
+
+
+
